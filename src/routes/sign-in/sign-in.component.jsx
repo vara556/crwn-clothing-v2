1ce1481e818429f5c5b3bed3562a147b4693c7dc
Varaprasad Veesam <varaprasadveesam@Varaprasads-MacBook-Air.local>
2022-09-04 14:29:20 +0530

    added the login with redirect

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,11 +1,26 @@
+import { getRedirectResult } from 'firebase/auth';
+import { useEffect } from 'react';
 import {
+  auth,
   signInWithGooglePopup,
   createUserProfileDocument,
+  signInWithGoogleRedirect
 } from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
+  useEffect(async() => {
+    const response = await getRedirectResult(auth);
+    if(response){
+
+      const userDocRef = await  createUserProfileDocument(response.user);
+    }
+  }, [])
   const logGoogleUser = async () => {
     const {user} = await signInWithGooglePopup();
+   const userDocRef = await  createUserProfileDocument(user);
+  };
+  const logGoogleUserWithRedirect = async () => {
+    const {user} = await signInWithGoogleRedirect();
    const userDocRef = await  createUserProfileDocument(user);
   };
 
